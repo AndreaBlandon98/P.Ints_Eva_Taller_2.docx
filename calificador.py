d6de2164210827e1cca966f9426ad9e54a341211
AndreaBlandon98 <160431077+AndreaBlandon98@users.noreply.github.com>
2024-03-07 19:30:13 -0500

    Add files via upload
    Actividades de producto taller 2

---
+++ calificador.py
@@ -0,0 +1,53 @@
+print("CALIFICADOR DE PROGRAMAS")
+print("[1] hola mundo")
+print("[2] suma de numeros")
+print("[3] calcular la edad")
+print("[4] IMC")
+print("[5] hallar el numero par e impar de un numero")
+print("[6] hallar el area de una cuadrado")
+print("[7] hallar el area de un triangulo")
+print("[8] validar si es mayor o menor de edad")
+print("[9] validar aumento de salario")
+print("[10] pago de horas extras")
+opcion = int(input("Elige una opcion: "))
+
+match opcion:
+           case 1:
+               print("hola mundo")
+               float(input("ingrese su calificacion de 1 a 5: "))
+           case 2:
+               print("suma de numeros")
+               float(input("ingrese su calificacion de 1 a 5: "))
+           case 3:
+               print("calcular la edad")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 4:
+               print("IMC")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 5:
+               print("hallar el numero par e impar de un numero")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 6:
+               print("hallar el area de una cuadrado")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 7:
+               print("hallar el area de un triangulo")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 8:
+               print("validar si es mayor o menor de edad")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 9:
+               print("validar aumento de salario")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case 10:
+               print("pago de horas extras")
+               float(input("ingrese su calificacion de 1 a 5:"))
+           case _:
+               print("opción no valida")
+               float(input("ingrese su calificacion de 1 a 5:"))
+
+
+
+
+
+
